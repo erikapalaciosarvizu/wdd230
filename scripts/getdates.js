c14a70bba10cb06365b35fd6b386c878ba2d52ba
erikapalaciosarvizu <erika.palacios.arvizu@gmail.com>
2023-09-30 22:43:05 -0600

    maps y hamburger menu

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -6,3 +6,18 @@ document.querySelector("#year").textContent = currentYear.getFullYear();
 /* LAST MODIFIED */
 let text = document.lastModified;
 document.getElementById("demo").innerHTML = text;
+
+
+function openHamMenu() { //La funcion openHamMenu se puede llamar como yo quiera
+	document.getElementById("mySidenav").style.width = "250px";
+	//document = objeto que me permite acceder a los objetos del HTML.
+	//getElementById = metodo o funcion que permite obtener el objeto del HTML por su ID.
+	//style = objeto con el que se accede al estilo del objeto previamente seleccionado por su ID. (CSS)
+	//width = Es un parametro dentro del estilo / medida de que aparezca, lo ancho del hamburger menu.
+  }
+  
+  function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+  }
+
+  
