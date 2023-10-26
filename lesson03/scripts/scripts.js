badb562465e4dfdc3d8286cccc95db185a09985b
erikapalaciosarvizu <erika.palacios.arvizu@gmail.com>
2023-10-26 02:10:15 -0600

    lazy load

---
+++ lesson03/scripts/scripts.js
@@ -0,0 +1,8 @@
+/* CURRENT YEAR */
+const currentYear = new Date;
+
+document.querySelector("#year").textContent = currentYear.getFullYear();
+
+/* LAST MODIFIED */
+let text = document.lastModified;
+document.getElementById("demo").innerHTML = text;
