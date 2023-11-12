ee3774c61aa074f1466ed273c5127f6391160459
erikapalaciosarvizu <erika.palacios.arvizu@gmail.com>
2023-11-11 22:11:28 -0600

    Discover page

--- chamber/scripts/getdateschamber.js
+++ chamber/scripts/getdateschamber.js
@@ -21,6 +21,9 @@ let text = document.lastModified;
 document.getElementById("lastModified").textContent = text;
 
 
+
+
+
 // JavaScript para activar/desactivar el modo oscuro
 const darkModeToggle = document.getElementById("darkModeToggle");
 const body = document.body;
@@ -34,3 +37,45 @@ darkModeToggle.addEventListener("click", () => {
     }
 });
 
+/* localStorage */
+  const msToDays = 86400000; // 24 * 60 * 60 * 1000
+
+  const sidebarContent = document.querySelector('.discover-sidebar-content');
+
+  function updateVisitDate() {
+    const currentDate = new Date();
+    localStorage.setItem("lastVisit", currentDate.toISOString());
+  }
+
+  function getDaysBetweenVisits() {
+    const lastVisit = localStorage.getItem("lastVisit");
+
+    if (!lastVisit) {
+      return null; // priera visita
+    }
+
+    const daysBetween = (new Date() - new Date(lastVisit)) / msToDays;
+    return daysBetween;
+  }
+
+  function displayMessage() {
+    const daysBetweenVisits = getDaysBetweenVisits();
+
+    if (daysBetweenVisits === null) {
+      // First visit
+      sidebarContent.innerHTML = `<p>Welcome! Let us know if you have any questions.</p>` + sidebarContent.innerHTML;
+    } else if (daysBetweenVisits < 1) {
+      // menos de un dia entre las visitas
+      sidebarContent.innerHTML = `<p>Back so soon! Awesome!</p>` + sidebarContent.innerHTML;
+    } else {
+      // mas de un dia
+      const pluralSuffix = daysBetweenVisits === 1 ? '' : 's';
+      sidebarContent.innerHTML = `<p>You last visited ${daysBetweenVisits.toFixed(0)} day${pluralSuffix} ago.</p>` + sidebarContent.innerHTML;
+    }
+
+    // Esto acualiza el dia de visita
+    updateVisitDate();
+  }
+
+  // llama el displayMessage en la pag
+  displayMessage();
