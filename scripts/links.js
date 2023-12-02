b55afd2f29c61e4719ec740d27d094dbac38cc2a
erikapalaciosarvizu <erika.palacios.arvizu@gmail.com>
2023-12-01 23:21:24 -0600

    JSON

--- scripts/links.js
+++ scripts/links.js
@@ -1,38 +1,30 @@
 const baseURL = "https://erikapalaciosarvizu.github.io/wdd230";
-
 const linksURL = "https://erikapalaciosarvizu.github.io/wdd230/data/links.json";
-const cards = document.querySelector('.div-activities');
-
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
     displayLinks(data.lessons);
- }
+}
 
- function displayLinks(weeks) {
-    const activitiesList = document.querySelector('.div-activities');
-    activitiesList.innerHTML = ''; // Limpiar el contenido existente
- 
+function displayLinks(weeks) {
+    const activitiesList = document.getElementById('learning-activities');
+    const listContainer = document.createElement('div'); // Contenedor para la lista
+    listContainer.className = 'activities-list-container'; // Clase para el contenedor
+
     weeks.forEach(week => {
-       const card = document.createElement('section');
-       card.className = 'card';
-       const h3 = document.createElement('h3');
-       h3.textContent = `Week ${week.lesson}`;
-       const ul = document.createElement('ul');
- 
-       week.links.forEach(link => {
-          const li = document.createElement('li');
-          const a = document.createElement('a');
-          a.href = `${baseURL}/${link.url}`;
-          a.textContent = `${week.lesson}: ${link.title}`;
-          li.appendChild(a);
-          ul.appendChild(li);
-       });
- 
-       card.appendChild(h3);
-       card.appendChild(ul);
-       activitiesList.appendChild(card);
+        week.links.forEach(link => {
+            const p = document.createElement('p');
+            const a = document.createElement('a');
+            a.href = `${baseURL}/${link.url}`;
+            a.textContent = `${week.lesson}: ${link.title}`;
+            p.appendChild(a);
+            listContainer.appendChild(p);
+        });
     });
- }  
-  getLinks();
+
+    activitiesList.appendChild(listContainer);
+}
+
+document.addEventListener('DOMContentLoaded', getLinks);
+
